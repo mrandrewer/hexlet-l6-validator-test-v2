4ad2ed61e4deb44db833f93bfee82c7055bcb642
Andrei Belskii <mrandrewer@yandex.ru>
2023-12-27 20:28:00 +0300

    Fix lint errors

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -8,21 +8,20 @@ class ObjectSchema {
   }
 
   shape(schema) {
-    const isPlainObject = o => o?.constructor === Object;
+    const isPlainObject = (o) => o?.constructor === Object;
     const iter = (val, sh) => {
       const oKeys = Object.keys(val);
       return Object.keys(sh).every((k) => {
-        if(!oKeys.includes(k)) return false;
-        if(!isPlainObject(sh[k])) {
+        if (!oKeys.includes(k)) return false;
+        if (!isPlainObject(sh[k])) {
           return sh[k].isValid(val[k]);
         }
-        return iter(val[k], sh[k])
+        return iter(val[k], sh[k]);
       });
     };
     const validator = (v) => iter(v, schema);
     return new ObjectSchema([...this.validators, validator]);
   }
-  
 }
 
 export default ObjectSchema;
